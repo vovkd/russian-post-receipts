ae151910df5ffa2fa3470b9f1c16955ed33ec678
s999 <oak@oak.by>
2014-02-13 21:23:28 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='pdfrender',
       version='0.1',
-      package_dir={'pdf_render': 'src/pdf_render'},
+      package_dir={'': 'src'},
       packages=['pdf_render',],
       #install_requires=open('requirements.txt').readlines(),
       )
